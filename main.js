5fe68d0810d50f4af7659c6f002231d1475e7fd2
artwwl <artwligocki@gmail.com>
2022-11-11 08:26:21 -0300

    dark mode beta

--- main.js
+++ main.js
@@ -58,3 +58,31 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var body = document.querySelector('body');
+  var darkModeToggle = document.querySelector('.dark-mode-toggle');
+
+  darkModeToggle.addEventListener('click', () => {
+    if (body.classList.contains('bright')){
+        document.documentElement.style.setProperty('--body-color', '#292929');
+        document.documentElement.style.setProperty('--base-color', 'rgb(235, 181, 4)');
+        document.documentElement.style.setProperty('--title-font-color', '#f1f1f1');
+        document.documentElement.style.setProperty('--subtitle-font-color', '#dddddd');
+        document.documentElement.style.setProperty('--border-color', 'var(--base-color)');
+        document.documentElement.style.setProperty('--box-shadow-color', 'rgba(0, 0, 0, 0.8)');
+        
+        body.classList.remove('bright');
+        body.classList.add('dark');
+    }else{
+        document.documentElement.style.setProperty('--body-color', '#F9F9F9');
+        document.documentElement.style.setProperty('--base-color', 'rgb(253, 194, 0)');
+        document.documentElement.style.setProperty('--title-font-color', '#241f0f');
+        document.documentElement.style.setProperty('--subtitle-font-color', '#767676');
+        document.documentElement.style.setProperty('--border-color', '#E4E4E4');
+        document.documentElement.style.setProperty('--box-shadow-color', 'rgba(0, 0, 0, 0.158)');
+        
+        body.classList.remove('dark');
+        body.classList.add('bright');
+    }
+    
+  })
